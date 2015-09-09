09b44c42251b55ceacc2c6b3df32087c27f955c2
Niko Wenselowski <der@nik0.de>
2015-09-09 22:01:19 +0200

    Supply a nicer string representation.

--- recipe.py
+++ recipe.py
@@ -36,6 +36,9 @@ class Category(db.Model):
     def __init__(self, name):
         self.name = name
 
+    def __str__(self):
+        return self.name
+
     def __repr__(self):
         return '<Category %r>' % self.name
 
