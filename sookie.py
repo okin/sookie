1280c3c60e2bee5305e91d3e63602e878b7b08bf
Niko Wenselowski <der@nik0.de>
2015-10-07 19:31:27 +0200

    Adding license APGLv3

--- sookie.py
+++ sookie.py
@@ -1,5 +1,21 @@
 # -*- coding: utf-8 -*-
 
+# sookie is a recipe and food scheduling web application
+# Copyright (C) 2015  Niko Wenselowski
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from flask import Flask, url_for, render_template, redirect, request
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.wtf import Form
